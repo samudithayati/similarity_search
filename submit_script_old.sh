edb4403f942ffda5028c5539d6e8167251b50f5c
samudithayati <94757449+samudithayati@users.noreply.github.com>
2024-01-07 01:44:18 -0500

    Update and rename distance.sh to submit_script_old.sh

--- submit_script_old.sh
+++ submit_script_old.sh
@@ -5,13 +5,13 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -p BigMem
-#SBATCH --mail-type=FAIL,END
-#SBATCH --mail-user=yatiwely@email.sc.edu
+##SBATCH --mail-type=FAIL,END
+##SBATCH --mail-user=
 
 ##Load your modules first:
 
 module load python3/anaconda/2021.07
-source activate /work/yatiwely/ENVS/rdkit
+source activate /work/username/ENVS/rdkit
 ##Add your code here:
 
 hostname
